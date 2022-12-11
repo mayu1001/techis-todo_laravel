694ec62dcca31c8df9935c3d3d8ca7233899f144
mayu1001 <moe2002.kou2005mama@gmail.com>
2022-12-11 16:43:22 +0900

    12/11

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->charset('utf-8')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
